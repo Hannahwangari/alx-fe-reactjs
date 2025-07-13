bcd7cde7a649a2eeaf268ad190dfe46cb72ea499
ANNABEL <annwanja950@gmail.com>
2025-07-13 02:57:14 -0500

    chore: initialize React project and add README

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello ALX!</h1>
+      <p>Welcome to your first custom React component 🚀</p>
+    </div>
+  )
+}
+
+export default App
