332cd75ce761b3a2e063d80d38e7f0b9f0e6fd51
ANNABEL <annwanja950@gmail.com>
2025-07-13 09:09:25 -0500

    feat: add UserProfile component and render with props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,18 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile'; // ✅ Import here
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+      <UserProfile
+        name="Hannah"
+        age="24"
+        bio="Frontend Developer and tech enthusiast learning React!"
+      />
       <Footer />
     </div>
   );
