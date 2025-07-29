248c09c8fae1c6df0f6c03c2384b267052fd1071
ANNABEL <annwanja950@gmail.com>
2025-07-29 06:17:09 -0500

    Add route for AddRecipeForm and component implementation

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import SearchBar from './components/SearchBar';
-import RecipeDetail from './components/RecipeDetail'; // create this dummy component
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <SearchBar />
         <Routes>
           <Route path="/" element={<RecipeList />} />
+          <Route path="/add" element={<AddRecipeForm />} />
           <Route path="/recipe/:id" element={<RecipeDetail />} />
         </Routes>
       </div>
