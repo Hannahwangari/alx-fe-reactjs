610088fbe4d36d7afea493d12b238990751c8d99
ANNABEL <annwanja950@gmail.com>
2025-08-03 16:22:54 -0500

    Set up Tailwind CSS and updated styles

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import Search from './components/Search';
-
 function App() {
   return (
-    <div className="App">
-      <Search />
+    <div className="p-8 bg-gray-100 min-h-screen">
+      <h1 className="text-4xl font-bold text-indigo-600 underline">
+        Tailwind is Working!
+      </h1>
     </div>
   );
 }
-
 export default App;
